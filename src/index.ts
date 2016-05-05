876f0e4cb524b6e7136d211262f28f3e6e900fee
Sam Rijs <srijs@airpost.net>
2016-05-05 11:25:48 +0200

    expose stream primitives on the top level

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@
 import {Context} from './core/ctx';
 import {Eff} from './core/eff';
 import {EffUtil} from './core/util';
-import * as stream from './stream';
+import {SinkInterface, Sink, Source} from './stream';
 
 export {
   Eff, Context,
   EffUtil,
-  stream
+  SinkInterface, Sink, Source
 }
